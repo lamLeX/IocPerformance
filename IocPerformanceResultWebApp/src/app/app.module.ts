92b69b2f77cf8faf724ff9e06b5946f35988f7c2
Lam Le <lam281990@gmail.com>
2017-08-06 23:09:01 -0700

    Add material design imports

--- IocPerformanceResultWebApp/src/app/app.module.ts
+++ IocPerformanceResultWebApp/src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MaterialDesignModule } from "./material-design/material-design.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    MaterialDesignModule
   ],
   providers: [],
   bootstrap: [AppComponent]
